7d3137bbf254e1406e8c34d011e22dc08be71598
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-09-15 02:28:42 +0000

    criando deposito

--- src/test/java/Deposito.java
+++ src/test/java/Deposito.java
@@ -9,12 +9,19 @@ import com.example.DigitalWallet;
 
 class Deposito {
         
+        @ParameterizedTest
+        @ValueSource(doubles = {0.01, 1.5, 27, 700})
         void deveDepositarValoresValidos(double amount) {
-            
+            DigitalWallet digitalWallet = new DigitalWallet("Ítalo", 700);
+            digitalWallet.deposit(amount);
+
+            assertEquals(digitalWallet.getBalance(), amount, 27);
         }
 
-        
+        @ParameterizedTest
+        @ValueSource(doubles = {0, -50, -250})
         void deveLancarExcecaoParaDepositoInvalido(double amount) {
-            
+            DigitalWallet digitalWallet = new DigitalWallet("Ítalo", 0.0);
+            assertThrows(IllegalArgumentException.class, () -> digitalWallet.deposit(amount));
         }
     }
