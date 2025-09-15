5e99bdb722fae3c1a6878332b227735447beb921
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-09-15 02:33:09 +0000

    criando o pagamento

--- src/test/java/Pagamento.java
+++ src/test/java/Pagamento.java
@@ -13,16 +13,35 @@ import com.example.DigitalWallet;
 
 public class Pagamento {
     
+     @ParameterizedTest
+    @CsvSource({"700, 250, true", "50, 100, false"})
     void pagamentoComCarteiraVerificadaENaoBloqueada(double inicial, double valor, boolean esperado) {
-        
+        DigitalWallet wallet = new DigitalWallet("Ítalo", inicial);
+        wallet.verify();
+
+        assumeTrue(wallet.isVerified());
+        assumeFalse(wallet.isLocked());
+        boolean payed = wallet.pay(valor);
+
+        assertEquals(payed, esperado);
     }
 
-    
+    @ParameterizedTest
+    @ValueSource(doubles = {0, -1.5, -27, -250.75})
     void deveLancarExcecaoParaPagamentoInvalido(double valor) {
-        
+        DigitalWallet wallet = new DigitalWallet("Ítalo", 700);
+        wallet.verify();
+
+        assumeTrue(wallet.isVerified());
+        assumeFalse(wallet.isLocked());
+
+        assertThrows(IllegalArgumentException.class, () -> wallet.pay(valor));
     }
 
+    @Test
     void deveLancarSeNaoVerificadaOuBloqueada() {
-        
+        DigitalWallet wallet = new DigitalWallet("Daniel", 250);
+
+        assertThrows(IllegalStateException.class, () -> wallet.pay(50));
     }
 }
