9c76c27b1eb705d0de32f6c29421071582169007
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-09-15 02:34:22 +0000

    criando saldo inicial

--- src/test/java/SaldoInicial.java
+++ src/test/java/SaldoInicial.java
@@ -6,11 +6,17 @@ import org.junit.jupiter.api.Test;
 import com.example.DigitalWallet;
 
 class SaldoInicial {
+     @Test
         void deveConfigurarSaldoInicialCorreto() {
-            
+            DigitalWallet wallet = new DigitalWallet("Ítalo", 700);
+            assertTrue(wallet.getBalance() > 0);
         }
 
+        @Test
         void deveLancarExcecaoParaSaldoInicialNegativo() {
-            
+            assertThrows(IllegalArgumentException.class, () -> {
+                new DigitalWallet("Ítalo", -250);
+            });
         }
+
     }
